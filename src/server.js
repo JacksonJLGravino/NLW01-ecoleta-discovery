098b85fc1f7c1dd9c55db426db549f12db12ec5e
Jackson Gravino <jacksonjlgravino@gmail.com>
2023-11-30 11:55:06 -0300

    aula 4

---
+++ src/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const server = express();
+const nunjucks = require("nunjucks");
+
+server.use(express.static("public"));
+
+nunjucks.configure("src/views", {
+  express: server,
+  noCache: true,
+});
+
+server.get("/", (req, res) => {
+  return res.render("index.html", { title: "um titulo" });
+});
+
+server.get("/create-point", (req, res) => {
+  return res.render("create-point.html");
+});
+
+server.get("/search", (req, res) => {
+  return res.render("search-results.html");
+});
+
+server.listen(3000);
